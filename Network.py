5b48bbd1e157ef28ecb8c245aebe619662333a2d
Alessandro Abluton <ablutonalessandro@gmail.com>
2020-06-10 16:56:14 +0200

    experiment

--- Network.py
+++ Network.py
@@ -13,23 +13,23 @@ class RegressiveCNN(nn.Module):
         # identificare 12 features
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()  # output.shape = 24x64x64
-        self.pool1 = nn.MaxPool2d(kernel_size=2)  # output.shape = 24x32x32
+        #self.pool1 = nn.MaxPool2d(kernel_size=2)  # output.shape = 24x32x32
 
         self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()  # output.shape = 24x32x32
-        self.pool2 = nn.MaxPool2d(kernel_size=2)  # output.shape = 24x16x16
+        #self.pool2 = nn.MaxPool2d(kernel_size=2)  # output.shape = 24x16x16
 
         self.conv3 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=3, stride=1, padding=1)
         self.relu3 = nn.ReLU()  # output.shape = 36x16x16
-        self.pool3 = nn.MaxPool2d(kernel_size=2)  # output.shape = 36x8x8
+        #self.pool3 = nn.MaxPool2d(kernel_size=2)  # output.shape = 36x8x8
 
         self.conv4 = nn.Conv2d(in_channels=96, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.relu4 = nn.ReLU()  # output.shape = 48x8x8
-        self.pool4 = nn.MaxPool2d(kernel_size=2)  # output.shape = 48x4x4
+        #self.pool4 = nn.MaxPool2d(kernel_size=2)  # output.shape = 48x4x4
 
         # qua comincia MLP, dopo layer di Flatten ho output.shape[1]*output.shape[2]*output.shape[3]
         # input features (vettore colonna) + 5 parametri meteo
-        self.fc1 = nn.Linear(in_features=128*4*4 + 7, out_features=256)
+        self.fc1 = nn.Linear(in_features=128*64*64 + 7, out_features=256)
         #self.dropout1 = nn.Dropout(0.2)
         self.fc2 = nn.Linear(in_features=256, out_features=256)
         #self.dropout2 = nn.Dropout(0.2)
@@ -44,25 +44,25 @@ class RegressiveCNN(nn.Module):
     def forward(self, x, weather):
         output = self.conv1(x)
         output = self.relu1(output)
-        output = self.pool1(output)
+        #output = self.pool1(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool1 --> {}'.format(output.shape))
 
         output = self.conv2(output)
         output = self.relu2(output)
-        output = self.pool2(output)
+        #output = self.pool2(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool2 --> {}'.format(output.shape))
 
         output = self.conv3(output)
         output = self.relu3(output)
-        output = self.pool3(output)
+        #output = self.pool3(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool3 --> {}'.format(output.shape))
 
         output = self.conv4(output)
         output = self.relu4(output)
-        output = self.pool4(output)
+        #output = self.pool4(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool4 --> {}'.format(output.shape))
 
