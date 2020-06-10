e9d4666a67e358814c213ca233ffccbaa012c311
Alessandro Abluton <ablutonalessandro@gmail.com>
2020-06-10 18:40:41 +0200

    experiment

--- CleanAirDataset.py
+++ CleanAirDataset.py
@@ -36,9 +36,21 @@ class CleanAirDataset(data.Dataset):
         if self.transform:
             image = self.transform(image)
 
-        weather = self.data_df.iloc[idx, 3:]
+        weather = self.data_df.iloc[idx, [0, 3, 4, 5, 6, 7, 8, 9]]
+
+        if type(weather['time']) == str:
+            repl = {
+                'Monday': 1, 'Thursday': 2, 'Wednesday': 3, 'Tuesday': 4,
+                'Friday': 5, 'Saturday': 6, 'Sunday': 7, 'January': 1,
+                'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+                'July': 7, 'August': 8, 'September': 9, 'October': 10,
+                'November': 11, 'December': 12, 'January': 1,
+                'WIN': 1, 'SPR': 2, 'SUM': 3, 'AUT': 4
+            }
+            weather = weather.replace(repl)
         # convert weather data to column vector
         weather = np.array(weather).astype('float32').reshape(-1, 1)
+        #print(weather)
         # convert to tensor
         weather = torch.from_numpy(weather)
 
