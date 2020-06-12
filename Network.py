75c34b6d1f0fd30dfe4d3e681af17edc3434d188
Alessandro Abluton <ablutonalessandro@gmail.com>
2020-06-12 14:47:11 +0200

    added batch normalization after each layer

--- Network.py
+++ Network.py
@@ -13,36 +13,51 @@ class RegressiveCNN(nn.Module):
         # identificare 12 features
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()  # output.shape = 12x64x64
+        self.bn1 = nn.BatchNorm2d(num_features=12)
         #self.pool1 = nn.MaxPool2d(kernel_size=2)  # output.shape = 12x32x32
 
         self.conv2 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()  # output.shape = 24x64x64
+        self.bn2 = nn.BatchNorm2d(num_features=24)
+
         #self.pool2 = nn.MaxPool2d(kernel_size=2)  # output.shape = 24x32x32
 
         self.conv3 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=3, stride=1, padding=1)
         self.relu3 = nn.ReLU()  # output.shape = 48x64x64
+        self.bn3 = nn.BatchNorm2d(num_features=24)
+
         #self.pool3 = nn.MaxPool2d(kernel_size=2)  # output.shape = 48x32x32
 
         self.conv4 = nn.Conv2d(in_channels=24, out_channels=12, kernel_size=3, stride=1, padding=1)
         self.relu4 = nn.ReLU()  # output.shape = 48x64x64
         self.pool4 = nn.MaxPool2d(kernel_size=2)  # output.shape = 48x32x32
+        self.bn4 = nn.BatchNorm2d(num_features=12)
+
 
         self.w1 = nn.Linear(in_features=8, out_features=32)
+        self.w_bn1 = nn.BatchNorm1d(num_features=32)
         self.w2 = nn.Linear(in_features=32, out_features=64)
+        self.w_bn2 = nn.BatchNorm1d(num_features=64)
         self.w3 = nn.Linear(in_features=64, out_features=64)
+        self.w_bn3 = nn.BatchNorm1d(num_features=64)
         self.w4 = nn.Linear(in_features=64, out_features=32)
+        self.w_bn4 = nn.BatchNorm1d(num_features=32)
         self.w5 = nn.Linear(in_features=32, out_features=8)
-        
+
         # layer Bilinear, B(x1, x2) = x1^t * M * x2 + b
         # M, b imparati da Bilinear, x1=feature map
         # x2=dati, dati = dati_meteo + coord_cella + indice_temporale
-        self.bilinear = nn.Bilinear(12*32*32, 8, 512)
+        self.bilinear = nn.Bilinear(12*32*32, 8, 2048)
+        self.mlp_bn1 = nn.BatchNorm1d(num_features=2048)
         #self.dropout1 = nn.Dropout(0.2)
-        self.fc1 = nn.Linear(in_features=512, out_features=512)
+        self.fc1 = nn.Linear(in_features=2048, out_features=1024)
+        self.mlp_bn2 = nn.BatchNorm1d(num_features=1024)
         #self.dropout2 = nn.Dropout(0.2)
-        self.fc2 = nn.Linear(in_features=512, out_features=512)
-        self.fc3 = nn.Linear(in_features=512, out_features=256)
-        self.fc4 = nn.Linear(in_features=256, out_features=1)
+        self.fc2 = nn.Linear(in_features=1024, out_features=2048)
+        self.mlp_bn3 = nn.BatchNorm1d(num_features=2048)
+        self.fc3 = nn.Linear(in_features=2048, out_features=1024)
+        self.mlp_bn4 = nn.BatchNorm1d(num_features=1024)
+        self.fc4 = nn.Linear(in_features=1024, out_features=1)
 
     def show_image(self, img):
         npimg = img
@@ -52,24 +67,28 @@ class RegressiveCNN(nn.Module):
         output = self.conv1(x)
         output = self.relu1(output)
         #output = self.pool1(output)
+        output = self.bn1(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool1 --> {}'.format(output.shape))
 
         output = self.conv2(output)
         output = self.relu2(output)
         #output = self.pool2(output)
+        output = self.bn2(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool2 --> {}'.format(output.shape))
 
         output = self.conv3(output)
         output = self.relu3(output)
         #output = self.pool3(output)
+        output = self.bn3(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool3 --> {}'.format(output.shape))
 
         output = self.conv4(output)
         output = self.relu4(output)
         output = self.pool4(output)
+        output = self.bn4(output)
         # self.show_image(to_pil(output[0].cpu()))
         # print('pool4 --> {}'.format(output.shape))
 
@@ -81,9 +100,13 @@ class RegressiveCNN(nn.Module):
         # i dati meteo passano per un MLP separato, in modo da permettere
         # al modello di pesarli singolarmente e in modo accurato
         weather = self.w1(weather)
+        weather = self.w_bn1(weather)
         weather = self.w2(weather)
+        weather = self.w_bn2(weather)
         weather = self.w3(weather)
+        weather = self.w_bn3(weather)
         weather = self.w4(weather)
+        weather = self.w_bn4(weather)
         weather = self.w5(weather)
 
         # print(output.shape)
@@ -91,11 +114,15 @@ class RegressiveCNN(nn.Module):
         #output = torch.cat((output, weather), dim=1)
 
         output = self.bilinear(output, weather)
+        output = self.mlp_bn1(output)
         #output = self.dropout1(output)
         output = self.fc1(output)
+        output = self.mlp_bn2(output)
         #output = self.dropout2(output)
         output = self.fc2(output)
+        output = self.mlp_bn3(output)
         output = self.fc3(output)
+        output = self.mlp_bn4(output)
         output = self.fc4(output)
         
         return output
